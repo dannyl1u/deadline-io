be859dd1bdcddf922699ab440426e67405d15ca8
dla216 <danny_liu_5@sfu.ca>
2023-03-11 11:59:31 -0800

    unadding files

--- server/server.js
+++ server/server.js
@@ -1,37 +1,26 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require ('dotenv').config();
-const jobRoutes = require('./routes/job.route');
+const connectDB = require('./config/db');
+var cors = require('cors');
+
+// routes
+const jobs = require('./routes/job.route');
 
 const app = express();
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 
-const rp = require('request-promise');
-const url = 'https://test-api-jobboard.workbc.ca/Test/JbSearch#/job-search';
+// Connect Database
+connectDB();
+// cors
+app.use(cors({ origin: true, credentials: true }));
 
-rp(url)
-  .then(function(html){
-    //success!
-    console.log(html);
-  })
-  .catch(function(err){
-    //handle error
-  });
+// Init Middleware
+app.use(express.json({ extended: false }));
 
-app.use('/jobs', jobRoutes);
-const PORT = process.env.PORT || 8000;
-// simple route
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to deadline server!" });
-    });
+app.get('/', (req, res) => res.send('Hello world!'));
 
-    
-app.listen(PORT, (req, res) => {
-    console.log(`Server is running on port ${PORT}.`);
-});
+// use Routes
+app.use('/api/jobs', jobs);
 
 
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
