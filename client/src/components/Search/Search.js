e1ce3e6cc55332303d3427f8a830e8279b162a26
dla216 <danny_liu_5@sfu.ca>
2023-03-12 03:42:17 -0700

    update search radio buttons

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -2,54 +2,29 @@ import React, { useState } from "react";
 import Chip from '@mui/joy/Chip';
 import "./search.scss";
 import Box from '@mui/joy/Box';
-import Avatar from '@mui/joy/Avatar';
 import LockIcon from '@mui/icons-material/Lock';
 import LockOpenIcon from '@mui/icons-material/LockOpen';
+import Checkbox from '@mui/material/Checkbox';
+import Radio from '@mui/joy/Radio';
+import RadioGroup from '@mui/joy/RadioGroup';
+import Typography from '@mui/joy/Typography';
 
+function Search() {
+    const [selected, setSelected] = React.useState('');
 
-function Sidebar() {
-    const [filterText, setFilterText] = useState("");
-    const [companies, setCompanies] = useState([
-      {
-        id: "sap",
-        label: "SAP",
-        value: false,
-        image: "../assets/images/amazon.png",
-      },
-      { id: "mastercard", label: "Mastercard", value: false, image: "meta.png" },
-      { id: "lululemon", label: "Lululemon", value: false, image: "apple.png" },
-      { id: "electronicarts", label: "Electronic Arts", value: false, image: "google.png" },
-    ]);
-  
-    const handleFilterChange = (event) => {
-      setFilterText(event.target.value);
-    };
-  
-    const handleCheckboxChange = (event) => {
-      const { id, checked } = event.target;
-      setCompanies((prevState) =>
-        prevState.map((company) =>
-          company.id === id ? { ...company, value: checked } : company
-        )
-      );
-    };
-  
-    const filteredCompanies = companies.filter((company) =>
-      company.label.toLowerCase().includes(filterText.toLowerCase())
-    );
-  
     return (
     <div>
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop:"30px", fontSize: "larger"}}>
-        Showing application deadlines for:
+        Showing job application deadlines for:
         </div>
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding:"10px"}}>
             <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
-                <Chip label="Chip Filled" endDecorator={<LockOpenIcon fontSize="md"/>}>BCIT</Chip>
+                <Chip label="Chip Filled" onClick={() => alert("test")} endDecorator={<LockOpenIcon fontSize="md"/>}>BCIT</Chip>
                 <Chip label="Chip Outlined" endDecorator={<LockIcon fontSize="md"/>} style={{ opacity: "50%" }} >Mastercard</Chip>
                 <Chip label="Chip Outlined" endDecorator={<LockIcon fontSize="md"/>} style={{ opacity: "50%" }} >Lululemon</Chip>
                 <Chip label="Chip Outlined" endDecorator={<LockIcon fontSize="md"/>} style={{ opacity: "50%" }} >Electronic Arts</Chip>
                 <Chip label="Chip Outlined" endDecorator={<LockIcon fontSize="md"/>} style={{ opacity: "50%" }} >SAP</Chip>
+                and more coming soon...
             </Box>
         </div>
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop:"10px"}}>
@@ -62,4 +37,4 @@ function Sidebar() {
     );
   }
   
-  export default Sidebar;
+  export default Search;
