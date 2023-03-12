848f49c0414c6805d7876031796008d738e6af2b
dla216 <danny_liu_5@sfu.ca>
2023-03-11 19:52:14 -0800

    display scraped jobs on calendar 📆

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,10 +1,39 @@
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+
 import React, { useState, useEffect } from "react";
-import Calendar from "react-calendar";
-import Sidebar from "./Sidebar";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "react-big-calendar/lib/css/react-big-calendar.css"
+import Sidebar from "./Sidebar"
+
+const locales = {
+  "en-US": require("date-fns/locale/en-US"),
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek, 
+  getDay,
+  locales,
+});
+
+const jobPostings = [
+  {
+      title: "SPONSORED: Mastercard - Software Engineer (iOS - Mobile)",
+      start: new Date("Mar 11, 2023"),
+      end: new Date("Mar 11, 2023"),
+  },
+];
 
 function Dashboard(props) {
-  const [value, onChange] = useState(new Date());
-  const [jobs, setJobs] = useState([]);
+    const [value, onChange] = useState(new Date());
+    const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:8000/getJobs")
@@ -13,30 +42,21 @@ function Dashboard(props) {
       })
       .then(function (data) {
         console.log(data);
+        for (const jobTitle in data) {
+          jobPostings.push({title: jobTitle, start: new Date(data[jobTitle]), end: new Date(data[jobTitle])})
+        }
         setJobs(data);
       })
       .catch(function (error) {});
   }, []); // add an empty dependency array to ensure that the effect is only run once
-
-  return (
-    <div>
-      <label htmlFor="companies">Companies</label>
+  
+  return (  
+    <div> 
       <Sidebar />
-      {/* <select name="companies" id="companies">
-        <option value="amazon">Amazon</option>
-        <option value="google">Google</option>
-        <option value="meta">Meta</option>
-        <option value="apple">Apple</option>
-      </select> */}
-      <Calendar onChange={onChange} value={value} />
-      {Object.values(jobs).map((job) => (
-        <div key={job.title}>
-          <h3>{job.title}</h3>
-          <p>{job.company}</p>
-        </div>
-      ))}
+      <Calendar localizer={localizer} events={jobPostings}
+        startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
     </div>
-  );
+  )
 }
 
 export default Dashboard;
