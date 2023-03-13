7dd68a35365240b3174d073728b7fcfc1c0de887
chantelle mckenzie <mrspasceri@hotmail.com>
2023-03-12 17:40:59 -0700

    removed env file

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-require('dotenv').config()
-const db = process.env.MONGO_URI;
+const db = require ('../config/keys').mongoURI;
 
 const connectDB = async () => {
   try {
